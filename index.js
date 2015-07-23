a92810ab9052bd162c6bdf32a9e1f48622b7a430
Timofey A. Anisimov <anisimovt@gmail.com>
2015-07-24 02:17:15 +0300

    bot completed

--- index.js
+++ index.js
@@ -1,29 +1,21 @@
 var keys = require('./keys');
 var tu = require('tuiter')(keys);
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/huyarl');
-var Schema = mongoose.Schema;
-var ObjectId = Schema.ObjectId;
-var Last = new Schema({
-	last: Number
-});
-
-var last_value;
 
 var task = function () {
-	if (!last_value) {
-		Last.findOne({}, function (err, res) {
-			if (err || !res) {
-				last_value = new Last({'last': 0});
-			} else {
-				last_value = res;
+	tu.filter({track: ['Карл', 'Карл!']}, function(stream) {
+		stream.on('tweet', function (data) {
+			if (!data.retweeted_status && (data.text.search(/\,( )?Карл/i) != -1)) {
+				tu.update({
+					status: '@' + data.user.screen_name + ' Хуярл!',
+					in_reply_to_status_id: data.id_str
+				}, function (err, tweet) {
+					if (err)
+						console.log('Err', err);
+				});
+				console.log('ID: ' + data.id_str + ',  Created: ' + data.created_at + ', Text: "' + data.text + '"\n');
 			}
-
-			tu.filter({track: [', Карл', ',Карл']}, function(stream) {
-				stream.on('tweet', function (data) {
-					console.log(data);
-				})
-			})
 		});
-	}
+	});
 }
+
+task();
