5b90ef8a7c4e722479f281e6d2b3932674631d4e
Timofey A. Anisimov <anisimovt@gmail.com>
2015-07-27 18:37:34 +0300

    remove some garbage

--- index.js
+++ index.js
@@ -1,21 +1,17 @@
 var keys = require('./keys');
 var tu = require('tuiter')(keys);
 
-var task = function () {
-	tu.filter({track: ['Карл', 'Карл!']}, function(stream) {
-		stream.on('tweet', function (data) {
-			if (!data.retweeted_status && (data.text.search(/\,( )?Карл/i) != -1)) {
-				tu.update({
-					status: '@' + data.user.screen_name + ' Хуярл!',
-					in_reply_to_status_id: data.id_str
-				}, function (err, tweet) {
-					if (err)
-						console.log('Err', err);
-				});
-				console.log('ID: ' + data.id_str + ',  Created: ' + data.created_at + ', Text: "' + data.text + '"\n');
-			}
-		});
+tu.filter({track: ['Карл', 'Карл!']}, function(stream) {
+	stream.on('tweet', function (data) {
+		if (!data.retweeted_status && (data.text.search(/\,( )?Карл/i) != -1)) {
+			tu.update({
+				status: '@' + data.user.screen_name + ' Хуярл!',
+				in_reply_to_status_id: data.id_str
+			}, function (err, tweet) {
+				if (err)
+					console.log('Err', err);
+			});
+			console.log('ID: ' + data.id_str + ',  Created: ' + data.created_at + ', Text: "' + data.text + '"\n');
+		}
 	});
-}
-
-task();
+});
